f0575a262d1ad389acce7be84ba92e7db4b3e1bd
liliang <liang2.li@cicc.com.cn>
2023-04-25 17:17:22 +0800

    chore: url转换小工具

--- components/oooooUrl.vue
+++ components/oooooUrl.vue
@@ -7,6 +7,77 @@
 import { ref } from 'vue';
 
 const transferStr = ref('https://yetching-blog.netlify.app');
+
+const encoded = ref(false);
+
+const toUTF8Array = (str) => {
+  var utf8 = [] as any;
+  for (var i = 0; i < str.length; i++) {
+    var charcode = str.charCodeAt(i);
+    if (charcode < 0x80) utf8.push(charcode);
+    else if (charcode < 0x800) {
+      utf8.push(0xc0 | (charcode >> 6), 0x80 | (charcode & 0x3f));
+    } else if (charcode < 0xd800 || charcode >= 0xe000) {
+      utf8.push(
+        0xe0 | (charcode >> 12),
+        0x80 | ((charcode >> 6) & 0x3f),
+        0x80 | (charcode & 0x3f)
+      );
+    } else {
+      i++;
+      charcode = ((charcode & 0x3ff) << 10) | (str.charCodeAt(i) & 0x3ff);
+      utf8.push(
+        0xf0 | (charcode >> 18),
+        0x80 | ((charcode >> 12) & 0x3f),
+        0x80 | ((charcode >> 6) & 0x3f),
+        0x80 | (charcode & 0x3f)
+      );
+    }
+  }
+  console.log(utf8, 'utf8');
+  return utf8;
+};
+
+const Utf8ArrayToStr = (array) => {
+  var out, i, len, c;
+  var char2, char3;
+
+  out = '';
+  len = array.length;
+  i = 0;
+  while (i < len) {
+    c = array[i++];
+    switch (c >> 4) {
+      case 0:
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+        // 0xxxxxxx
+        out += String.fromCharCode(c);
+        break;
+      case 12:
+      case 13:
+        // 110x xxxx   10xx xxxx
+        char2 = array[i++];
+        out += String.fromCharCode(((c & 0x1f) << 6) | (char2 & 0x3f));
+        break;
+      case 14:
+        // 1110 xxxx  10xx xxxx  10xx xxxx
+        char2 = array[i++];
+        char3 = array[i++];
+        out += String.fromCharCode(
+          ((c & 0x0f) << 12) | ((char2 & 0x3f) << 6) | ((char3 & 0x3f) << 0)
+        );
+        break;
+    }
+  }
+
+  return out;
+};
 </script>
 
 <style scoped></style>
